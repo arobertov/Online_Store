453693e2637b81d3630337c706fb9be3b586caaf
AngelRobertov <gelerob@dir.bg>
2018-08-25 18:22:43 +0300

    Add new functionality

--- src/ShopBundle/Form/ProductType.php
+++ src/ShopBundle/Form/ProductType.php
@@ -7,7 +7,6 @@ use ShopBundle\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
